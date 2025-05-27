e659c5edd72771e83ef9503610de56b8478f49f5
Joanna Schmidt <joanna.n.schmidt@nasa.gov>
2025-05-27 15:48:45 -0400

    removed except

--- uqpce/__init__.py
+++ uqpce/__init__.py
@@ -1,9 +1,9 @@
 __version__ = '1.0.0'
 
 from uqpce.pce.pce import PCE
-try:
-    from uqpce.mdao.uqpcegroup import UQPCEGroup
-    import examples.paraboloid.paraboloid.paraboloid as paraboloid
-    from uqpce.mdao import interface
-except:
-    ImportError # openmdao not installed, which is fine if analysis-only is desired
+# try:
+from uqpce.mdao.uqpcegroup import UQPCEGroup
+import examples.paraboloid.paraboloid.paraboloid as paraboloid
+from uqpce.mdao import interface
+# except ImportError:
+#     pass # openmdao not installed, which is fine if analysis-only is desired
